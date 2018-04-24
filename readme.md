00188ad48424d771b938fb287d25c7c706119c71
Sander van de Velde <sander@vandevelde-online.com>
2018-04-24 22:00:00 +0200

    initial

---
+++ readme.md
@@ -0,0 +1,44 @@
+﻿# Advantech Wise4012e Modbus Simulation
+
+Application which simulates an Advantech Wise 4012e IO module. The application is written in .Net Core.
+
+## Purpose
+
+It runs a Modbus service at [127.0.0.1] (configurable):
+
+* Switch 1 on Discrete coil 1 (read/write) 0 or 1
+* Switch 2 on Discrete coil 2 (read/write) 0 or 1
+* Relay 1 on Discrete coil 17 (read) 0 or 1
+* Relay 2 on Discrete coil 18 (read) 0 or 1
+* Knob 1 on Holding Register 40001 (read/write) 0 - 4500
+* Knob 2 on Holding Register 40002 (read/write) 0 - 4500 
+
+## Usage
+
+Compile and run this application in [Visual studio Code](https://code.visualstudio.com/).
+
+You can read and write Modbus using this simulation in Modbus clients like [CAS Modbus Scanner](http://store.chipkin.com/articles/modbus-scanner-what-is-the-cas-modbus-scanner) or [Azure IoT Edge](https://docs.microsoft.com/en-us/azure/iot-edge/deploy-modbus-gateway).
+
+![alt tag](img/simulation.jpg)
+
+## Alter behaviour
+
+When running the application, you can alter the values of the simulation.
+
+Just enter:
+
+* 1 0 (switch to false)
+* 1 1 (switch to true)
+* 2 0 (switch to false)
+* 2 2 (switch to true)
+* 40001 0-4500 (pick a value between 0 and 4500)
+* 40002 0-4500 (pick a value between 0 and 4500)
+
+## Contribute
+
+This logic is licenced under the MIT license.
+
+Want to contribute? Throw me a pull request....
+
+Want to know more about me? Check out my (blog)[http://blog.vandevelde-online.com]
+
---
+++ src/Advantech.Wise4012e.Modbus.SimulationApp/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Extensions.Configuration;
+using Modbus.Data;
+using Modbus.Device;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Advantech.Wise4012e.Modbus.SimulationApp
+{
+    internal class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        private static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("settings.json");
+
+            Configuration = builder.Build();
+
+            var address = Configuration["address"];
+
+            Console.WriteLine($"Start Advantech Wise4012e Modbus simulation at {address}");
+
+            var slaveTcpListener = new TcpListener(IPAddress.Parse(address), 502);
+
+            slaveTcpListener.Start();
+
+            byte SlaveID = 1;
+
+            var slave = ModbusTcpSlave.CreateTcp(SlaveID, slaveTcpListener);
+            slave.DataStore = DataStoreFactory.CreateDefaultDataStore();
+
+            slave.DataStore.DataStoreReadFrom += DataStore_DataStoreReadFrom;
+            slave.DataStore.DataStoreWrittenTo += DataStore_DataStoreWrittenTo;
+
+            slave.DataStore.HoldingRegisters[1] = (ushort)0;
+            slave.DataStore.HoldingRegisters[2] = (ushort)0;
+
+            slave.DataStore.CoilDiscretes[1] = false;
+            slave.DataStore.CoilDiscretes[2] = false;
+            slave.DataStore.CoilDiscretes[17] = false;
+            slave.DataStore.CoilDiscretes[18] = false;
+
+            slave.Listen();
+
+            Console.WriteLine($"Slave {SlaveID} supports switch 1, switch 2, relay 17, relay 18, knob 40001, knob 40002");
+
+            while (true)
+            {
+                Console.WriteLine($"> {slave.DataStore.CoilDiscretes[1]} {slave.DataStore.CoilDiscretes[2]} {slave.DataStore.CoilDiscretes[17]} {slave.DataStore.CoilDiscretes[18]} {slave.DataStore.HoldingRegisters[1]:0000} {slave.DataStore.HoldingRegisters[2]:0000} - Write '1 [bit]' '2 [bit]' '40001 [int]' '40002 [int]' or Q to exit");
+                Console.Write("> ");
+
+                var input = Console.ReadLine();
+
+                if (input.ToUpper() == "Q")
+                {
+                    break;
+                }
+
+                var fields = input.Split(' ');
+
+                switch (fields[0])
+                {
+                    case "1":
+                        slave.DataStore.CoilDiscretes[1] = Convert.ToBoolean(Convert.ToInt16(fields[1]));
+                        break;
+
+                    case "2":
+                        slave.DataStore.CoilDiscretes[2] = Convert.ToBoolean(Convert.ToInt16(fields[1]));
+                        break;
+
+                    case "40001":
+                        var value1 = Math.Abs(Convert.ToInt32(fields[1]));
+                        slave.DataStore.HoldingRegisters[1] = value1 > 4500 ? (ushort)4500 : (ushort)value1;
+                        break;
+
+                    case "40002":
+                        var value2 = Math.Abs(Convert.ToInt32(fields[1]));
+                        slave.DataStore.HoldingRegisters[2] = value2 > 4500 ? (ushort)4500 : (ushort)value2;
+                        break;
+                }
+            }
+        }
+
+        private static void DataStore_DataStoreReadFrom(object sender, DataStoreEventArgs e)
+        {
+            {
+                if (e.ModbusDataType == ModbusDataType.HoldingRegister)
+                {
+                    Console.WriteLine($"\nClient reads {e.ModbusDataType} address {e.StartAddress + 40001} - read {e.Data.B[0]}");
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.WriteLine($"\nClient reads {e.ModbusDataType} address {e.StartAddress + 1} - read {e.Data.A[0]}");
+                    Console.Write("> ");
+                }
+            }
+        }
+
+        private static void DataStore_DataStoreWrittenTo(object sender, DataStoreEventArgs e)
+        {
+            Console.WriteLine($"\nClient writes {e.ModbusDataType} address {e.StartAddress + 1} - write {e.Data.A[0]}");
+            Console.Write("> ");
+        }
+    }
+}
